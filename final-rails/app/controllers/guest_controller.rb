659b506548c157276c24f84e029995ce218854d4
Thomas Dalgetty <tdalgetty0213@gmail.com>
2024-11-22 16:42:58 -0700

    shows playlists to guest users

--- final-rails/app/controllers/guest_controller.rb
+++ final-rails/app/controllers/guest_controller.rb
@@ -1,7 +1,17 @@
 class GuestController < ApplicationController
+  before_action :set_playlists 
   def index
     if user_signed_in?
       redirect_to playlists_path()
     end
   end
+
+  private
+    def set_playlists
+      @playlists = Playlist.all
+    end
+
+    def playlist_params
+      params.require(:playlist).permit(:title, :description)
+    end
 end
